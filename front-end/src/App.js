05968419efdcf510849d5308a63cbae596002771
shiyas <shiyasali.23@gmail.com>
2023-11-09 03:57:05 +0530

    Landing page done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Introduction from './Components/Introduction/Introduction';
+import Store from './Components/Store/Store';
+import Header from './Components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Introduction/>}/>
+        <Route path="/store" element={<Store/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ front-end/src/Components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <h1>
+            SKY <br />
+            DOT
+          </h1>
+        </div>
+        <div className="nav-list">
+          <ul>
+           <Link style={{ textDecoration: 'none' }} to='/'>
+           <li>Home</li>
+           </Link> 
+            <li>Connect</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+        <div className="nav-cart-icon">
+          <p className="cart-quantity"></p>
+          <i class="cart-icon fa-solid fa-cart-shopping"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
