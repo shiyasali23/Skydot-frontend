3dcdf77f9b5b006f6c14a71c1560c7237be2c637
shiyas <shiyasali.23@gmail.com>
2023-11-11 03:53:27 +0530

    cart updation p4

--- front-end/src/Components/BagIcon/BagIcon.jsx
+++ front-end/src/Components/BagIcon/BagIcon.jsx
@@ -1,10 +1,19 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { cartContext } from "../../Contexts/CartProvider";
 import all_products from "../products";
 
 const BagIcon = ({ index, id }) => {
-  const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(() => {
+    // Retrieve the clicked state from localStorage on component mount
+    const storedClicked = localStorage.getItem(`bagIconClicked_${id}`);
+    return storedClicked ? JSON.parse(storedClicked) : false;
+  });
   const { cartArray, setCartArray } = useContext(cartContext);
+
+  useEffect(() => {
+    // Save the clicked state to localStorage whenever it changes
+    localStorage.setItem(`bagIconClicked_${id}`, JSON.stringify(clicked));
+  }, [clicked, id]);
 
   const handleClick = () => {
     setClicked((prevClicked) => !prevClicked);
